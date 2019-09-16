91252afed6d5c2c447da2de446d5a0171c8bcea1
Charles Hessel <hesselcharles@gmail.com>
2019-09-16 17:19:08 +0200

    test and profile the new c function

--- fpc_run.py
+++ fpc_run.py
@@ -2,8 +2,11 @@
 
 import iio
 import numpy as np
+import timeit
+
 # from s2p import triangulation
-from fpc import filter_xyz
+from fpc import filter_xyz, filter_xyz_bis
+
 
 image = iio.read('test_image.tif')  # iio use 3rd dim for color channels
 image = image.squeeze().astype('float32')
@@ -24,11 +27,24 @@ xyz_array[:, :, 2] = 100 + image * 50
 # xyz_array = xyz_array.astype('float32')
 
 r = 3.0  # filtering radius, in meters
-n = 2  # number of points (under which a pixel is rejected)
+n = 8  # number of points (under which a pixel is rejected)
 img_gsd = 1
 
 # triangulation.filter_xyz(xyz_array, r, n, img_gsd)
-filter_xyz(xyz_array, r, n, img_gsd)
+
+# start = timeit.default_timer()
+# filter_xyz(xyz_array, r, n, img_gsd)
+# end = timeit.default_timer()
+# print('filter_xyz, time = ', end - start, ' seconds')
+
+# start = timeit.default_timer()
+filter_xyz_bis(xyz_array, r, n, img_gsd)
+# end = timeit.default_timer()
+# print('filter_xyz_bis, time = ', end - start, ' seconds')
+
+# profiling conclusion:
+# filter_xyz_bis where the NAN assignement is done with the c function is not
+# faster, maybe even slightly slower.
 
 output = xyz_array[:, :, 2].squeeze()
 easting = xyz_array[:, :, 0].squeeze()
