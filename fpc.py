f52d169f00f2a1aa84152c7dd861a2b8543ce0ab
Charles Hessel <hesselcharles@gmail.com>
2019-09-16 17:18:22 +0200

    create python ctypes interface with new c function

--- fpc.py
+++ fpc.py
@@ -38,6 +38,27 @@ def count_3d_neighbors(xyz, r, p):
     return out
 
 
+def remove_isolated_3d_points(xyz, r, p, n):
+    """ Remove isolated pixels or group of pixels in a gridded set of 3D points.
+
+    Args:
+        xyz (array): 3D array of shape (h, w, 3) where each pixel contains the
+            UTM easting, northing, and altitude of a 3D point.
+        r (float): filtering radius, in meters
+        p (int): the filering window has size 2p + 1, in pixels
+    """
+    h, w, d = xyz.shape
+    assert d == 3, 'expecting a 3-channels image with shape (h, w, 3)'
+
+    lib.remove_isolated_3d_points.argtypes = (
+        ndpointer(dtype=c_int, shape=(h, w)),
+        ndpointer(dtype=c_float, shape=(h, w, 3)),
+        c_int, c_int, c_float, c_int, c_int)
+
+    out = np.zeros((h, w), dtype='int32')
+    lib.remove_isolated_3d_points(out, xyz, w, h, r, p, n)
+
+
 def filter_xyz(xyz, r, n, img_gsd):
     """
     Discard (in place) points that have less than n points closer than r meters.
@@ -52,4 +73,12 @@ def filter_xyz(xyz, r, n, img_gsd):
     p = np.ceil(r / img_gsd).astype(int)
     count = count_3d_neighbors(xyz, r, p)
     xyz[count < n] = np.nan
+
+
+def filter_xyz_bis(xyz, r, n, img_gsd):
+    """ The new version of filter_xyz
+    """
+    p = np.ceil(r / img_gsd).astype(int)
+    remove_isolated_3d_points(xyz, r, p, n)
+
 
