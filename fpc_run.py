4e0e6dcf60440c484eae6c248df43f4355c2649a
Charles Hessel <hesselcharles@gmail.com>
2019-09-18 14:48:55 +0200

    test and profile

--- fpc_run.py
+++ fpc_run.py
@@ -4,7 +4,6 @@ import iio
 import numpy as np
 import timeit
 
-# from s2p import triangulation
 from fpc import filter_xyz, filter_xyz_bis
 
 
@@ -18,40 +17,23 @@ xyz_array[:, :, 1] = np.broadcast_to(np.transpose(
                      np.arange(h, dtype='float32')[np.newaxis], (1, 0)), (h, w))
 xyz_array[:, :, 2] = 100 + image * 50
 
-# xyz_array = np.array(
-#         [np.broadcast_to(np.arange(image.shape[1], dtype=np.float32), image.shape[0:2]),
-#          np.broadcast_to(np.transpose(
-#              np.arange(image.shape[0], dtype=np.float32)[np.newaxis], (1, 0)), image.shape[0:2]),
-#          image*255]
-#         ).transpose((1, 2, 0))
-# xyz_array = xyz_array.astype('float32')
-
-r = 3.0  # filtering radius, in meters
-n = 8  # number of points (under which a pixel is rejected)
+r = 4.0  # filtering radius, in meters
+n = 10  # number of points (under which a pixel is rejected)
 img_gsd = 1
 
-# triangulation.filter_xyz(xyz_array, r, n, img_gsd)
 
-# start = timeit.default_timer()
 # filter_xyz(xyz_array, r, n, img_gsd)
-# end = timeit.default_timer()
-# print('filter_xyz, time = ', end - start, ' seconds')
-
-# start = timeit.default_timer()
 filter_xyz_bis(xyz_array, r, n, img_gsd)
-# end = timeit.default_timer()
-# print('filter_xyz_bis, time = ', end - start, ' seconds')
 
-# profiling conclusion:
-# filter_xyz_bis where the NAN assignement is done with the c function is not
-# faster, maybe even slightly slower.
 
 output = xyz_array[:, :, 2].squeeze()
 easting = xyz_array[:, :, 0].squeeze()
 northing = xyz_array[:, :, 1].squeeze()
 
-# iio.write('test_out.tif', xyz_array)
 iio.write('test_out.tif', (output - 100) / 50 )
 iio.write('test_easting.tif', easting)
 iio.write('test_northing.tif', northing)
 
+# profiling conclusion:
+# filter_xyz_bis where the NAN assignement is done with the c function is not
+# faster, maybe even slightly slower that filter_xyz (from s2p)
